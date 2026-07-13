6430bc45e74a2ef93000b713ff17b612186a67e8
kev_bot <kevismakingthings@gmail.com>
2026-07-13 01:51:10 -0700

    switching to other comp

--- scripts/object_logic/color_rect.gd
+++ scripts/object_logic/color_rect.gd
@@ -6,7 +6,7 @@ var c2_time: float = 0.0
 var c1_freq: float = -0.1
 var c2_freq: float = 0.0
 var rate: float = 0
-var time_to_start: float = 9.8
+var time_to_start: float = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if material is ShaderMaterial:
@@ -38,12 +38,14 @@ func _sync() -> void:
 	#mat.set_shader_parameter("color_a", Color(.6,.8,.6))
 	#mat.set_shader_parameter("color_b", Color(.6,.8,.6))
 	var t = create_tween().set_parallel().set_loops()
-	t.tween_property(self, "c1_freq", 3, 1.05).set_ease(Tween.EASE_IN)
-	t.tween_property(self, "c1_freq", 0.5, 1.05).from(0).set_ease(Tween.EASE_IN).set_delay(1.05)
+	t.tween_property(self, "c1_freq", 3, 1.05).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
+	t.tween_property(self, "c2_freq", 1.05, 0).from(.5).set_ease(Tween.EASE_OUT)
+	t.tween_property(self, "c1_freq", 2, 1.05).from(-0.5).set_ease(Tween.EASE_IN_OUT).set_delay(1.05)
 	t.tween_property(self, "c2_freq", 0, 1.05).from(.5).set_ease(Tween.EASE_OUT).set_delay(1.05)
-	var t2 = create_tween().set_parallel().set_loops()
-	t2.tween_method(func(v): mat.set_shader_parameter("shift", v), .5, 0, 1.05).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE).set_delay(1.05)
-	t2.tween_method(func(v): mat.set_shader_parameter("rings2", v), 5, 12, 1.05).set_ease(Tween.EASE_OUT).set_delay(1.05)
-	t2.tween_method(func(v): mat.set_shader_parameter("color_b", v), Color(1,1,1,0.3), Color(0.12,0.1,0.1,0.6), 1.05).set_ease(Tween.EASE_OUT).set_delay(1.05)
+	await get_tree().create_timer(1.05).timeout
+	var t2 = create_tween().set_parallel().set_loops().set_trans(Tween.TRANS_QUART)
+	t2.tween_method(func(v): mat.set_shader_parameter("shift", v), .5, 0, 2.1).set_ease(Tween.EASE_OUT)
+	t2.tween_method(func(v): mat.set_shader_parameter("rings2", v), 5, 12, 2.1).set_ease(Tween.EASE_OUT)
+	t2.tween_method(func(v): mat.set_shader_parameter("color_b", v), Color(1,1,1,0.3), Color(randf_range(0,0.1),randf_range(0,0.1),randf_range(0,0.1),1), 2.1).set_ease(Tween.EASE_OUT)
 
 	
--- scripts/scene_logic/startmenu.gd
+++ scripts/scene_logic/startmenu.gd
@@ -20,13 +20,14 @@ func _on_aboutbutton_pressed() -> void:
 func _ready() -> void:               
 	var mat := container.material as ShaderMaterial
 	mat.set_shader_parameter("pixel_size", 32)
-	var t := create_tween().set_parallel()
-	t.tween_method(func(v): mat.set_shader_parameter("pixel_size", v), 32, 1, 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
-	t.tween_method(func(v): mat.set_shader_parameter("blur_amount", v), 1.5, 0, 1).set_ease(Tween.EASE_OUT)
+	mat.set_shader_parameter("blur_amount", 2)	
+	var t := create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
+	t.tween_method(func(v): mat.set_shader_parameter("pixel_size", v), 16, 1, 1.2).set_ease(Tween.EASE_OUT)
+	t.tween_method(func(v): mat.set_shader_parameter("blur_amount", v), 1, 0, 1.2).set_ease(Tween.EASE_OUT)
 
 func exit() -> void:
 	var mat := container.material as ShaderMaterial
 	var t := create_tween().set_parallel().set_process_mode(Tween.TWEEN_PROCESS_IDLE)
-	t.tween_method(func(v): mat.set_shader_parameter("pixel_size", v), 1, 12, 1).set_ease(Tween.EASE_IN_OUT)
+	t.tween_method(func(v): mat.set_shader_parameter("pixel_size", v), 1, 16, 1).set_ease(Tween.EASE_IN_OUT)
 	t.tween_method(func(v): mat.set_shader_parameter("blur_amount", v), 0.0, 1.3, 1).set_ease(Tween.EASE_IN_OUT)
 	await t.finished
